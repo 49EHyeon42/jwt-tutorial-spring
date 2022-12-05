e1638ef648f46e7519787a9e6056526e3f51db8e
49ehyeon42 <49ehyeon42@gmail.com>
2022-12-05 13:37:31 +0900

    init: spring boot project

---
+++ src/main/java/com/ehyeon/jwttutorial/JwtTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.ehyeon.jwttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTutorialApplication.class, args);
+	}
+}
